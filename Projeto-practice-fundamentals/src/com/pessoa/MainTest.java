29b83d2721c9f1f6fc29567de5e6b57118164858
Joicemar <33008360+Joicemar@users.noreply.github.com>
2022-12-03 13:41:47 -0300

    Add files via upload

--- Projeto-practice-fundamentals/src/com/pessoa/MainTest.java
+++ Projeto-practice-fundamentals/src/com/pessoa/MainTest.java
@@ -4,7 +4,17 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		
+		Pessoa magoFogo = new Mago("Jose", 32, Estado.VIP);
+		Pessoa guerreiro = new Guerreiro("Luiz", 32, Estado.VIP);
+		
 		Registrar registrar = new Registrar();
+		
+		registrar.listaPessoas.add( magoFogo);
+		registrar.listaPessoas.add( guerreiro);
+		
+		registrar.save(new Mago("Bruxo", 23, Estado.NO_VIP));
+		
 		registrar.load();
+		
 	}
 }
